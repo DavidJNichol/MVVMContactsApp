ecbb41aacd5116189a2e052f7bafb4eeb68c0966
David Nichol <david.nichol@loop.colum.edu>
2020-09-27 12:01:16 -0500

    Add project files.

---
+++ ContactsApp/ContactsApp/Services/MockDataStore.cs
@@ -0,0 +1,56 @@
+﻿using ContactsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Services
+{
+    public class MockDataStore : IDataStore<Contact>
+    {
+        readonly List<Contact> contacts;
+
+        public MockDataStore()
+        {
+            contacts = new List<Contact>()
+            {
+                new Contact { Id = Guid.NewGuid().ToString(), FirstName = "David", LastName="Gerding", Description="Teaches codes..." },
+                
+            };
+        }
+
+        public async Task<bool> AddItemAsync(Contact contact)
+        {
+            contacts.Add(contact);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Contact contact)
+        {
+            var oldItem = contacts.Where((Contact arg) => arg.Id == contact.Id).FirstOrDefault();
+            contacts.Remove(oldItem);
+            contacts.Add(contact);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = contacts.Where((Contact arg) => arg.Id == id).FirstOrDefault();
+            contacts.Remove(oldItem);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Contact> GetItemAsync(string id)
+        {
+            return await Task.FromResult(contacts.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Contact>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(contacts);
+        }
+    }
+}
---
+++ ContactsApp/ContactsApp/ViewModels/NewContactViewModel.cs
@@ -0,0 +1,72 @@
+﻿using ContactsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ContactsApp.ViewModels
+{
+    public class NewContactViewModel : BaseViewModel
+    {
+        private string firstName = string.Empty;
+        private string lastName = string.Empty;
+        private string description = string.Empty;
+
+        public NewContactViewModel()
+        {
+            SaveCommand = new Command(OnSave, ValidateSave);
+            CancelCommand = new Command(OnCancel);
+            this.PropertyChanged +=
+                (_, __) => SaveCommand.ChangeCanExecute();
+        }
+
+        private bool ValidateSave()
+        {
+            return !String.IsNullOrWhiteSpace(firstName)
+                && !String.IsNullOrWhiteSpace(lastName);
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set => SetProperty(ref firstName, value);
+        }
+        public string LastName
+        {
+            get => lastName;
+            set => SetProperty(ref lastName, value);
+        }
+
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+
+        public Command SaveCommand { get; }
+        public Command CancelCommand { get; }
+
+        private async void OnCancel()
+        {
+            // This will pop the current page off the navigation stack
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnSave()
+        {
+            Contact newContact = new Contact()
+            {
+                Id = Guid.NewGuid().ToString(),
+                FirstName= FirstName,
+                LastName= LastName,
+                Description = Description
+            };
+
+            await DataStore.AddItemAsync(newContact);
+
+            // This will pop the current page off the navigation stack
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
